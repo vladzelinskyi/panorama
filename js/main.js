2977d115d57fbe20488c55b22297bb216683d4bb
Vlad Zelinskyi <88655939+vladzelinskyi@users.noreply.github.com>
2024-04-07 15:43:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const panorama1 = new PANOLENS.ImagePanorama("../img/1.jpg");
+const panorama2 = new PANOLENS.ImagePanorama("../img/2.jpg");
+const imageContainer = document.querySelector(".panorama");
+
+const infospotPos = [new THREE.Vector3(3000, 0, 0), new THREE.Vector3(-2700, -500, -2500)];
+
+const viewer = new PANOLENS.Viewer({
+    container: imageContainer,
+    autoRotate: false,
+    autoRotateSpeed: 0.3,
+    controlBar: true,
+    horizontalView: true,
+});
+
+panorama1.link(panorama2, infospotPos[0]);
+panorama2.link(panorama1, infospotPos[1]);
+
+viewer.add(panorama1, panorama2);
